dabf72475bd5455a4c25a1f8eb5a795f4e85262e
Kelsey Dabrowski <kdabrowski103@gmail.com>
2026-03-01 01:37:19 -0500

    Add peek, size, and __repr__ functionality to queue

--- Assignment 1/stack.py
+++ Assignment 1/stack.py
@@ -25,8 +25,7 @@ class Stack:
     # pop method 
     def pop(self):
         if self.is_empty():
-            print("Cannot complete operation - stack is empty")
-            return
+            return "Cannot complete operation - stack is empty"
         else: 
             # Saves current node before we update top
             current_top = self.top
@@ -35,15 +34,43 @@ class Stack:
             return current_top.data
 
     # peek method 
+    def peek(self):
+        if self.is_empty():
+            return "Stack is empty"
+        else: 
+            print(self.top.data)
+            return
 
     # size method 
+    def size(self): 
+        if self.is_empty(): 
+            return "Stack is empty"
+        else: 
+            # Create counter
+            size = 0 
+            current_node = self.top
+            while current_node is not None: 
+                #print("Adding to counter")
+                size += 1
+                current_node = current_node.next
+            return size 
 
     # Make repr to print all the nodes
+    # I just modified this from the size() method
     def __repr__(self):
-        print(self.top)
-        ...
+        if self.is_empty():
+            return "Stack is empty"
+        else: 
+            print(f"Stack size: {self.size()}")
+            current_node = self.top
+            # I kept this outside of the while loop so I 
+            # could add the 'top' designation in the printout
+            print(f"{current_node.data} - top")
+            current_node = current_node.next
+            while current_node is not None: 
+                print(current_node.data)
+                current_node = current_node.next
     
-
 # Example 
 
 def main(): 
@@ -52,8 +79,6 @@ def main():
 
     print(stack.is_empty()) # Returns True
 
-    stack.__repr__()
-
     stack.pop() # Returns 'Cannot complete operation - stack is empty'
 
     stack.push(1)
@@ -61,5 +86,14 @@ def main():
     print(stack.pop()) # 2
     print(stack.pop()) # 1
     print(stack.pop()) # Returns 'Cannot complete operation - stack is empty'
+
+    stack.push(1) 
+    stack.peek() # 1
+    stack.push(2)
+    stack.push(3)
+
+    print(stack.size()) # 3 
+
+    stack.__repr__() # This will show a printout of the stack
 
 main()
